ba6bce539af67188f0ebe00c607fc21eb84b7d97
brahim lmlilas <spedibra@gmail.com>
2025-02-27 10:04:26 +0100

    banar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -57,8 +57,9 @@
             <div class="space-y-6">
                 <div class="bg-white rounded-xl shadow-sm overflow-hidden">
                     <div class="relative">
-                        <div class="h-24 bg-gradient-to-r from-blue-600 to-blue-400"></div>
-                        <img src="{{ asset('storage/' . $user->image) }}" alt="Profile"
+                        <div class="h-24 bg-gradient-to-r from-blue-600 to-blue-400 relative overflow-hidden">
+                            <img src="{{ asset('storage/'.$user->banner) }}" alt="Description" class="absolute inset-0 w-full h-full object-cover">
+                        </div>                        <img src="{{ asset('storage/' . $user->image) }}" alt="Profile"
                              class="absolute -bottom-6 left-4 w-20 h-20 rounded-full border-4 border-white shadow-md"/>
                     </div>
                     <div class="pt-14 p-4">

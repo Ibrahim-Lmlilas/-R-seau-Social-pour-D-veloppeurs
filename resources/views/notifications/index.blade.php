7b08ba89c44d63fc2b72eeb07e86954ddae91d4c
brahim lmlilas <spedibra@gmail.com>
2025-03-05 11:50:09 +0100

    notification V-18

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -52,7 +52,9 @@
                                                 <form action="{{ route('notifications.mark-read', $notification) }}" method="POST">
                                                     @csrf
                                                     <button type="submit" class="px-3 py-1 bg-gray-200 dark:bg-gray-600 rounded-md hover:bg-gray-300 dark:hover:bg-gray-500 text-sm">
-                                                        {{ __('Mark as read') }}
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                                                        </svg>
                                                     </button>
                                                 </form>
                                             @endif
@@ -60,7 +62,9 @@
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" class="px-3 py-1 bg-red-500 text-white rounded-md hover:bg-red-600 text-sm">
-                                                    {{ __('Delete') }}
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                    </svg>
                                                 </button>
                                             </form>
                                         </div>
